0c6da3aba40537a7bd2d8b18be0965c08b59e9c5
Dmytro Sakharchuk <sakharchukcoder@gmail.com>
2025-04-09 17:51:18 +0300

    log all the errors

--- front/js/main.js
+++ front/js/main.js
@@ -107,7 +107,11 @@
                 reportError(err);
 
                 document.querySelector('.fav-page').style.display = 'none';
-                window.location.href = '/promos/promo/stub/';
+                if (window.location.href.startsWith("https://www.favbet.hr/")) {
+                    window.location.href = '/promocije/promocija/stub/';
+                } else {
+                    window.location.href = '/promos/promo/stub/';
+                }
 
                 return Promise.reject(err);
             });
